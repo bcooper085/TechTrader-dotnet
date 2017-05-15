5518f8748079965c2857c1fe5eb6ff31fc8c3b6b
bcooper085 <bcooper085@gmail.com>
2017-05-15 14:52:20 -0700

    add bootstrap and css to forms and links

--- src/TechTrader/Startup.cs
+++ src/TechTrader/Startup.cs
@@ -46,6 +46,14 @@ namespace TechTrader
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
