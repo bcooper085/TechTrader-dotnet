55e156603d9e1d092d28672d782df00dff91af07
bcooper085 <bcooper085@gmail.com>
2017-05-15 10:55:09 -0700

    add base mvc files with post, newsletter and mailing list, shared view layout with bootstrap

---
+++ src/TechTrader/Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechTrader.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public virtual DbSet<MailingList> MailingLists { get; set; }
+        public virtual DbSet<Newsletter> Newsletters { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TechTrader;integrated security=True");
+        }
+    }
+}
---
+++ src/TechTrader/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechTrader.Models
+{
+    [Table("Posts")]
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Category { get; set; }
+        public string Location { get; set; }
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Post() { }
+    }
+}
